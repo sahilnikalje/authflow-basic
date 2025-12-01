6a21ce11699f5e9524994edd87f0951062953f20
sahilnikalje <sahilnik88@gmail.com>
2025-12-01 22:52:35 +0530

    routes added

---
+++ Backend/routes/authRoutes.js
@@ -0,0 +1,15 @@
+const express=require('express')
+const auth=require('../controllers/authController')
+const protect=require('../middleware')
+
+//express.Router is a function which is used to creates the routes in different fies 
+const router=express.Router()
+
+//public routes
+router.post('/register', auth.registerUser)
+router.post('/login', auth.loginUser)
+
+//private routes
+router.get('/profile', auth.getProfile)
+
+module.exports=router
