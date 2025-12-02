570ce6f08dca54544ac724bb2ec5b7cac67ffdf0
sahilnikalje <sahilnik88@gmail.com>
2025-12-02 18:56:45 +0530

    homepage route added

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -5,6 +5,11 @@ const protect=require('../middleware/authMiddleware')
 //express.Router is a function which is used to creates the routes in different fies 
 const router=express.Router()
 
+//route for homepage
+app.get('/', (req, res) => {
+  res.send('Authflow-basic backend is running 🚀');
+});
+
 //public routes
 router.post('/register', auth.registerUser)
 router.post('/login', auth.loginUser)
